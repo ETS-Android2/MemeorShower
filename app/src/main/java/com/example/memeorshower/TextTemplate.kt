278b7c2f004469a8fcf41b44ee4aac7dff8a3b21
narges <nsodeifi78@gmail.com>
2021-11-04 20:24:20 +0330

    Refactor TextTemplate page to have an image besides each text on the list

--- app/src/main/java/com/example/memeorshower/TextTemplate.kt
+++ app/src/main/java/com/example/memeorshower/TextTemplate.kt
@@ -8,20 +8,23 @@ import androidx.recyclerview.widget.RecyclerView
 
 class TextTemplate : AppCompatActivity() {
     lateinit var texts: ArrayList<String>
+    lateinit var images: ArrayList<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_template)
 
         val texts = ArrayList<String>()
+        val images = ArrayList<Int>()
         for (i in 1..20) {
             texts.add("Asbbbb"+ i)
+            images.add(R.mipmap.ic_launcher)
         }
 
         val text_lists = findViewById<View>(R.id.text_list) as RecyclerView
         // Initialize contacts
         // Create adapter passing in the sample user data
-        val adapter = RecyclerAdapter(texts)
+        val adapter = TextTemplateAdapter(texts, images)
         // Attach the adapter to the recyclerview to populate items
         text_lists.adapter = adapter
         // Set layout manager to position the items
--- app/src/main/java/com/example/memeorshower/TextTemplateAdapter.kt
+++ app/src/main/java/com/example/memeorshower/TextTemplateAdapter.kt
@@ -3,14 +3,15 @@ package com.example.memeorshower
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter (private var titles: List<String>):
-RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+class TextTemplateAdapter (private var titles: List<String>, private var images: List<Int>):
+RecyclerView.Adapter<TextTemplateAdapter.ViewHolder>(){
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val itemTitle: TextView = itemView.findViewById(R.id.textView1)
-//        val itemDetail: TextView = itemView.findViewById(R.id.text_list)
+        val itemImage: ImageView = itemView.findViewById(R.id.imageView1)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,6 +21,7 @@ RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.itemTitle.text = titles[position]
+        holder.itemImage.setImageResource(images[position])
 //        holder.itemDetail.text = titles[position]
     }
 
