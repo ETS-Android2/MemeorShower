73b0530915f2ead9ae35dbc2aa062719652b7604
narges <nsodeifi78@gmail.com>
2021-11-04 13:05:05 +0330

    Add binding for buildFeatures

--- app/src/main/java/com/example/memeorshower/MainActivity.kt
+++ app/src/main/java/com/example/memeorshower/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.memeorshower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.memeorshower.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.NewButton.setOnClickListener { makeNewMeme() }
+        binding.MyProjectButton.setOnClickListener { showMyProjects() }
+        binding.DatabaseButton.setOnClickListener { showDatabase() }
+
+
+    }
+
+    private fun showDatabase() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showMyProjects() {
+        TODO("Not yet implemented")
+    }
+
+    private fun makeNewMeme() {
+        TODO("Not yet implemented")
     }
 }
