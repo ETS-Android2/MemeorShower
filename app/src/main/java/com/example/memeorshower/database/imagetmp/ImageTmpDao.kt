b5b13b7f9899b7445a04fe50e3c54b2c1d20bd84
Niki Pourazin <nikipourzin@gmail.com>
2021-12-10 14:07:46 +0330

    Add Image template's table and Dao

--- app/src/main/java/com/example/memeorshower/database/imagetmp/ImageTmpDao.kt
+++ app/src/main/java/com/example/memeorshower/database/imagetmp/ImageTmpDao.kt
@@ -1,4 +1,19 @@
 package com.example.memeorshower.database.imagetmp
 
-class ImageTmpDao {
+import androidx.room.*
+
+@Dao
+interface ImageTmpDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsertByReplacement(image: List<ImageTmp>)
+
+    @Query("SELECT * FROM image_tmp_table ORDER BY id ASC")
+    fun getAll(): List<ImageTmp>
+
+    @Query("SELECT * FROM image_tmp_table WHERE id = :id ")
+    fun getByID(id: String): ImageTmp
+
+    @Delete
+    fun delete(imgtmp: ImageTmp)
 }
